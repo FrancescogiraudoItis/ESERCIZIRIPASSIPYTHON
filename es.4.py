5d87679444e06016d4e0d533a49d2b637ead7d73
Francesco <giraudofrancesco076@gmail.com>
2024-09-19 09:22:19 +0200

    es.4 finito

--- es.4.py
+++ es.4.py
@@ -1,6 +1,5 @@
 #esercizio 4 Dizionario: {"a": 1, "b": 2, "c": 3}, scambio la posizione tra chiave e valore
 Dizionario={"a": 1, "b": 2, "c": 3}
-#scambiare la posizione tra chiave e valore in un altro dizionario
 Dizionario2={}
 for chiave, valore in Dizionario.items():
     Dizionario2[valore]=chiave
